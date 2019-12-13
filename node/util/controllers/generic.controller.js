6f34a67dbe19192c4ce5e1a10c7c1e2f00064b68
Anthony <anthony.doucet@northorca.com>
2019-12-12 16:39:14 -0800

    generic controllers done without error handling

--- node/util/controllers/generic.controller.js
+++ node/util/controllers/generic.controller.js
@@ -1,28 +1,55 @@
+function getQuery() {
+    return res.locals.query || res.query || {};
+}
+function getBody() {
+    return res.locals.body || res.body;
+}
 const genericController = {
     get(service) {
         return async (req, res, next) => {
-            //get query object from res
-            next();
-        }
+            try {
+                let query = getQuery();
+                res.locals.data = await service(query);
+                next();
+            } catch (e) {
+                next(e);
+            }
+        };
     },
     put(service) {
         return async (req, res, next) => {
-            //get query and body
-            next();
-        }
+            try {
+                let query = getQuery();
+                let body = getBody();
+                res.locals.data = await service(query, body);
+                next();
+            } catch (e) {
+                next(e);
+            }
+        };
     },
     post(service) {
         return async (req, res, next) => {
-            //get body
-            next();
-        }
+            try {
+                let body = getBody();
+                res.locals.data = await service(body);
+                next();
+            } catch (e) {
+                next(e);
+            }
+        };
     },
     remove(service) {
         return async (req, res, next) => {
-            //get
-            next();
-        }
-    },
+            try {
+                let query = getQuery();
+                res.locals.data = await service(query);
+                next();
+            } catch (e) {
+                next(e);
+            }
+        };
+    }
 };
 
 module.exports = genericController;
